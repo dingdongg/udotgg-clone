04bd6216fd7d74fd3e4d3b373420dcb7c434964a
dingdongg <cs.dg.kim@gmail.com>
2022-08-21 18:14:04 -0700

    refactor searchForPlayer function

--- src/App.js
+++ src/App.js
@@ -1,58 +1,23 @@
 import './App.css';
 import React, { useState } from "react";
 import axios from 'axios';
-import IMAGES from './images.js';
-
-
+import playerQueryService from './services/player';
 
 function App() {
   const [searchText, setSearchText] = useState ("");
   const [playerData, setPlayerData] = useState ({});
   const [playerRank, setPlayerRank] = useState ("");
-  const API_KEY=  "RGAPI-e42d85ec-d3bc-487f-9573-ee6f5f5ae9d5";
   console.log(searchText)
 
   //function to search player
-  function searchForPlayer(event){
-    let APICallString="https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/"+ searchText+"?api_key="+API_KEY;
-    console.log(APICallString)
-    axios.get(APICallString)
-    .then(function(response){
-
-      setPlayerData(response.data)
-      
-      
-      
-    }).catch((error)=> console.log(error))
-   }
-
-   //function to print the stuff from data given
-  //  function printFromData(){
-  //   console.log("Agfasdfasdf" , playerData)
-  //    return JSON.stringify(playerData) != '{}' ? 
-
-     
-  //        <>
-        
-  //       <p>{playerData.name}</p>
-  //       <p>{playerData.summonerLevel}</p>
-  //       <img width = "100" height="100" src ={"http://ddragon.leagueoflegends.com/cdn/12.15.1/img/profileicon/"+ playerData.profileIconId +".png"}></img>
-  //       <img width = "100" height="100" src = {'"'+ printIconByRank (playerData.id)+ '"'} alt = "icon"></img>
-  //       {/* <img width = "100" height="100" src = "images/Emblem_Challenger.png" alt = "icon"></img> */}
-  //       {/* <p>{playerRank}</p> */}
-  //       </>
-       
-  //       :
-
-  //       <><p>No player Data</p></> 
-        
-         
-  //  }
+  function searchForPlayer(event) {
+    setPlayerData(playerQueryService.get(searchText));
+  }
 
    // this function is another api call which takes in the encripted riot id then from it, 
    //will have the string of the rank and print icon associated with it
    function printIconByRank(riotid){
-    let APICallString ="https://na1.api.riotgames.com/lol/league/v4/entries/by-summoner/"+ riotid +"?api_key="+API_KEY
+    let APICallString ="https://na1.api.riotgames.com/lol/league/v4/entries/by-summoner/"+ riotid +"?api_key="+process.env.API_KEY;
     axios.get(APICallString).then(function(response){
 
     
@@ -67,7 +32,7 @@ function App() {
         let responseData = response.data[i]
         console.log("test",responseData.queueType)
       
-       if (responseData.queueType == "RANKED_SOLO_5x5"){
+       if (responseData.queueType === "RANKED_SOLO_5x5"){
         hasMatch = true;
         setIndex = i
         break;
@@ -119,19 +84,16 @@ function App() {
         return "./images/Emblem_Gold.png"
       case 'PLATINUM':
         return "./images/Emblem_Platinum.png"
-       
       case 'DIAMOND':
-        image =  IMAGES.diamond
-        break;
-      
+        return "./images/Emblem_Diamond.png"
       case 'MASTER':
         return "./images/Emblem_Master.png"
       case 'GRANDMASTER':
         return "./images/Emblem_Grandmaster.png"  
       case 'CHALLENGER':
-        image = IMAGES.challenger
+        return "./images/Emblem_Challenger.png";
+      default:
         break;
-
     }
 
     return image
@@ -166,7 +128,7 @@ function App() {
           <button onClick={e => searchForPlayer(e)} >Search For Player</button>
         </div>
 
-    {JSON.stringify(playerData) != '{}' ? 
+    {JSON.stringify(playerData) !== '{}' ? 
 
      
 <>
@@ -175,7 +137,7 @@ function App() {
 <p>{playerData.summonerLevel}</p>
 
 
-<img width = "100" height="100" src ={"http://ddragon.leagueoflegends.com/cdn/12.15.1/img/profileicon/"+ playerData.profileIconId +".png"}></img>
+<img width = "100" height="100" src ={"http://ddragon.leagueoflegends.com/cdn/12.15.1/img/profileicon/"+ playerData.profileIconId +".png"} alt='profile icon'></img>
 <img width = "100" height="100" src = { printIconByRank (playerData.id)} alt = "icon"></img>
 {/* <img width = "100" height="100" src = {IMAGES.challenger} alt = "icon"></img> */}
 <p>{playerRank}</p>
