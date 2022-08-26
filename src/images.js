2a59f064d339df3111db3156f70f5893ad56aa82
Michael <michaeltandyo@gmail.com>
2022-08-26 00:23:16 -0600

    new branch

---
+++ src/images.js
@@ -0,0 +1,14 @@
+
+const IMAGES = {
+    iron: require("./images/rank_icons/Emblem_Iron.png"),
+    bronze:require("./images/rank_icons/Emblem_Bronze.png"),
+    silver:require("./images/rank_icons/Emblem_Silver.png"),
+    gold:require("./images/rank_icons/Emblem_Gold.png"),
+    platinum:require("./images/rank_icons/Emblem_Platinum.png"),
+    diamond:require("./images/rank_icons/Emblem_Diamond.png"),
+    master:require("./images/rank_icons/Emblem_Master.png"),
+    grandmaster:require("./images/rank_icons/Emblem_Grandmaster.png"),
+    challenger:require("./images/rank_icons/Emblem_Challenger.png" )
+}
+
+export default IMAGES;
